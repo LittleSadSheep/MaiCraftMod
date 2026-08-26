e7981038a35e2c4d454b95c825415f5e2321710b
LittleSadSheep <2749533087@qq.com>
2026-08-26 22:47:54 +0800

    feat: 提供 Baritone 选区编辑命令

---
+++ common/src/main/java/baritone/command/defaults/SelCommand.java
@@ -0,0 +1,300 @@
+/*
+ * This file is part of Baritone.
+ *
+ * Baritone is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Baritone is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Baritone.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package baritone.command.defaults;
+
+import baritone.Baritone;
+import baritone.api.IBaritone;
+import baritone.api.command.Command;
+import baritone.api.command.argument.IArgConsumer;
+import baritone.api.command.datatypes.ForAxis;
+import baritone.api.command.datatypes.ForBlockOptionalMeta;
+import baritone.api.command.datatypes.ForDirection;
+import baritone.api.command.datatypes.RelativeBlockPos;
+import baritone.api.command.exception.CommandException;
+import baritone.api.command.exception.CommandInvalidStateException;
+import baritone.api.command.exception.CommandInvalidTypeException;
+import baritone.api.command.helpers.TabCompleteHelper;
+import baritone.api.event.events.RenderEvent;
+import baritone.api.event.listener.AbstractGameEventListener;
+import baritone.api.schematic.*;
+import baritone.api.schematic.mask.shape.CylinderMask;
+import baritone.api.schematic.mask.shape.SphereMask;
+import baritone.api.selection.ISelection;
+import baritone.api.selection.ISelectionManager;
+import baritone.api.utils.BetterBlockPos;
+import baritone.api.utils.BlockOptionalMeta;
+import baritone.api.utils.BlockOptionalMetaLookup;
+import baritone.utils.BlockStateInterface;
+import baritone.utils.IRenderer;
+import baritone.utils.schematic.StaticSchematic;
+import com.mojang.blaze3d.vertex.BufferBuilder;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.core.Vec3i;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.AABB;
+
+import java.awt.*;
+import java.util.*;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
+import java.util.stream.Stream;
+
+public class SelCommand extends Command {
+
+    private ISelectionManager manager = baritone.getSelectionManager();
+    private BetterBlockPos pos1 = null;
+    private ISchematic clipboard = null;
+    private Vec3i clipboardOffset = null;
+
+    public SelCommand(IBaritone baritone) {
+        super(baritone, "sel", "selection", "s");
+        baritone.getGameEventHandler().registerEventListener(new AbstractGameEventListener() {
+            @Override
+            public void onRenderPass(RenderEvent event) {
+                if (!Baritone.settings().renderSelectionCorners.value || pos1 == null) {
+                    return;
+                }
+                Color color = Baritone.settings().colorSelectionPos1.value;
+                float opacity = Baritone.settings().selectionOpacity.value;
+                float lineWidth = Baritone.settings().selectionLineWidth.value;
+                boolean ignoreDepth = Baritone.settings().renderSelectionIgnoreDepth.value;
+                BufferBuilder bufferBuilder = IRenderer.startLines(color, opacity, lineWidth, ignoreDepth);
+                IRenderer.emitAABB(bufferBuilder, event.getModelViewStack(), new AABB(pos1));
+                IRenderer.endLines(bufferBuilder, ignoreDepth);
+            }
+        });
+    }
+
+    @Override
+    public void execute(String label, IArgConsumer args) throws CommandException {
+        Action action = Action.getByName(args.getString());
+        if (action == null) {
+            throw new CommandInvalidTypeException(args.consumed(), "an action");
+        }
+        if (action == Action.POS1 || action == Action.POS2) {
+            if (action == Action.POS2 && pos1 == null) {
+                throw new CommandInvalidStateException("Set pos1 first before using pos2");
+            }
+            BetterBlockPos playerPos = ctx.viewerPos();
+            BetterBlockPos pos = args.hasAny() ? args.getDatatypePost(RelativeBlockPos.INSTANCE, playerPos) : playerPos;
+            args.requireMax(0);
+            if (action == Action.POS1) {
+                pos1 = pos;
+                logDirect("Position 1 has been set");
+            } else {
+                manager.addSelection(pos1, pos);
+                pos1 = null;
+                logDirect("Selection added");
+            }
+        } else if (action == Action.CLEAR) {
+            args.requireMax(0);
+            pos1 = null;
+            logDirect(String.format("Removed %d selections", manager.removeAllSelections().length));
+        } else if (action == Action.UNDO) {
+            args.requireMax(0);
+            if (pos1 != null) {
+                pos1 = null;
+                logDirect("Undid pos1");
+            } else {
+                ISelection[] selections = manager.getSelections();
+                if (selections.length < 1) {
+                    throw new CommandInvalidStateException("Nothing to undo!");
+                } else {
+                    pos1 = manager.removeSelection(selections[selections.length - 1]).pos1();
+                    logDirect("Undid pos2");
+                }
+            }
+        } else if (action.isFillAction()) {
+            BlockOptionalMeta type = action == Action.CLEARAREA
+                    ? new BlockOptionalMeta(Blocks.AIR)
+                    : args.getDatatypeFor(ForBlockOptionalMeta.INSTANCE);
+
+            final BlockOptionalMetaLookup replaces; // Action.REPLACE
+            final Direction.Axis alignment;         // Action.(H)CYLINDER
+            if (action == Action.REPLACE) {
+                args.requireMin(1);
+                List<BlockOptionalMeta> replacesList = new ArrayList<>();
+                replacesList.add(type);
+                while (args.has(2)) {
+                    replacesList.add(args.getDatatypeFor(ForBlockOptionalMeta.INSTANCE));
+                }
+                type = args.getDatatypeFor(ForBlockOptionalMeta.INSTANCE);
+                replaces = new BlockOptionalMetaLookup(replacesList.toArray(new BlockOptionalMeta[0]));
+                alignment = null;
+            } else if (action == Action.CYLINDER || action == Action.HCYLINDER) {
+                args.requireMax(1);
+                alignment = args.hasAny() ? args.getDatatypeFor(ForAxis.INSTANCE) : Direction.Axis.Y;
+                replaces = null;
+            } else {
+                args.requireMax(0);
+                replaces = null;
+                alignment = null;
+            }
+            ISelection[] selections = manager.getSelections();
+            if (selections.length == 0) {
+                throw new CommandInvalidStateException("No selections");
+            }
+            BetterBlockPos origin = selections[0].min();
+            CompositeSchematic composite = new CompositeSchematic(0, 0, 0);
+            for (ISelection selection : selections) {
+                BetterBlockPos min = selection.min();
+                origin = new BetterBlockPos(
+                        Math.min(origin.x, min.x),
+                        Math.min(origin.y, min.y),
+                        Math.min(origin.z, min.z)
+                );
+            }
+            for (ISelection selection : selections) {
+                Vec3i size = selection.size();
+                BetterBlockPos min = selection.min();
+
+                // Java 8 so no switch expressions 😿
+                UnaryOperator<ISchematic> create = fill -> {
+                    final int w = fill.widthX();
+                    final int h = fill.heightY();
+                    final int l = fill.lengthZ();
+
+                    switch (action) {
+                        case WALLS:
+                            return new WallsSchematic(fill);
+                        case SHELL:
+                            return new ShellSchematic(fill);
+                        case REPLACE:
+                            return new ReplaceSchematic(fill, replaces);
+                        case SPHERE:
+                            return MaskSchematic.create(fill, new SphereMask(w, h, l, true).compute());
+                        case HSPHERE:
+                            return MaskSchematic.create(fill, new SphereMask(w, h, l, false).compute());
+                        case CYLINDER:
+                            return MaskSchematic.create(fill, new CylinderMask(w, h, l, true, alignment).compute());
+                        case HCYLINDER:
+                            return MaskSchematic.create(fill, new CylinderMask(w, h, l, false, alignment).compute());
+                        default:
+                            // Silent fail
+                            return fill;
+                    }
+                };
+
+                ISchematic schematic = create.apply(new FillSchematic(size.getX(), size.getY(), size.getZ(), type));
+                composite.put(schematic, min.x - origin.x, min.y - origin.y, min.z - origin.z);
+            }
+            baritone.getBuilderProcess().build("Fill", composite, origin);
+            logDirect("Filling now");
+        } else if (action == Action.COPY) {
+            BetterBlockPos playerPos = ctx.viewerPos();
+            BetterBlockPos pos = args.hasAny() ? args.getDatatypePost(RelativeBlockPos.INSTANCE, playerPos) : playerPos;
+            args.requireMax(0);
+            ISelection[] selections = manager.getSelections();
+            if (selections.length < 1) {
+                throw new CommandInvalidStateException("No selections");
+            }
+            BlockStateInterface bsi = new BlockStateInterface(ctx);
+            BetterBlockPos origin = selections[0].min();
+            CompositeSchematic composite = new CompositeSchematic(0, 0, 0);
+            for (ISelection selection : selections) {
+                BetterBlockPos min = selection.min();
+                origin = new BetterBlockPos(
+                        Math.min(origin.x, min.x),
+                        Math.min(origin.y, min.y),
+                        Math.min(origin.z, min.z)
+                );
+            }
+            for (ISelection selection : selections) {
+                Vec3i size = selection.size();
+                BetterBlockPos min = selection.min();
+                BlockState[][][] blockstates = new BlockState[size.getX()][size.getZ()][size.getY()];
+                for (int x = 0; x < size.getX(); x++) {
+                    for (int y = 0; y < size.getY(); y++) {
+                        for (int z = 0; z < size.getZ(); z++) {
+                            blockstates[x][z][y] = bsi.get0(min.x + x, min.y + y, min.z + z);
+                        }
+                    }
+                }
+                ISchematic schematic = new StaticSchematic(blockstates);
+                composite.put(schematic, min.x - origin.x, min.y - origin.y, min.z - origin.z);
+            }
+            clipboard = composite;
+            clipboardOffset = origin.subtract(pos);
+            logDirect("Selection copied");
+        } else if (action == Action.PASTE) {
+            BetterBlockPos playerPos = ctx.viewerPos();
+            BetterBlockPos pos = args.hasAny() ? args.getDatatypePost(RelativeBlockPos.INSTANCE, playerPos) : playerPos;
+            args.requireMax(0);
+            if (clipboard == null) {
+                throw new CommandInvalidStateException("You need to copy a selection first");
+            }
+            baritone.getBuilderProcess().build("Fill", clipboard, pos.offset(clipboardOffset));
+            logDirect("Building now");
+        } else if (action == Action.EXPAND || action == Action.CONTRACT || action == Action.SHIFT) {
+            args.requireExactly(3);
+            TransformTarget transformTarget = TransformTarget.getByName(args.getString());
+            if (transformTarget == null) {
+                throw new CommandInvalidStateException("Invalid transform type");
+            }
+            Direction direction = args.getDatatypeFor(ForDirection.INSTANCE);
+            int blocks = args.getAs(Integer.class);
+            ISelection[] selections = manager.getSelections();
+            if (selections.length < 1) {
+                throw new CommandInvalidStateException("No selections found");
+            }
+            selections = transformTarget.transform(selections);
+            for (ISelection selection : selections) {
+                if (action == Action.EXPAND) {
+                    manager.expand(selection, direction, blocks);
+                } else if (action == Action.CONTRACT) {
+                    manager.contract(selection, direction, blocks);
+                } else {
+                    manager.shift(selection, direction, blocks);
+                }
+            }
+            logDirect(String.format("Transformed %d selections", selections.length));
+        }
+    }
+
+    @Override
+    public Stream<String> tabComplete(String label, IArgConsumer args) throws CommandException {
+        if (args.hasExactlyOne()) {
+            return new TabCompleteHelper()
+                    .append(Action.getAllNames())
+                    .filterPrefix(args.getString())
+                    .sortAlphabetically()
+                    .stream();
+        } else {
+            Action action = Action.getByName(args.getString());
+            if (action != null) {
+                if (action == Action.POS1 || action == Action.POS2) {
+                    if (args.hasAtMost(3)) {
+                        return args.tabCompleteDatatype(RelativeBlockPos.INSTANCE);
+                    }
+                } else if (action.isFillAction()) {
+                    if (args.hasExactlyOne() || action == Action.REPLACE) {
+                        while (args.has(2)) {
+                            args.get();
+                        }
+                        return args.tabCompleteDatatype(ForBlockOptionalMeta.INSTANCE);
+                    } else if (args.hasExactly(2) && (action == Action.CYLINDER || action == Action.HCYLINDER)) {
+                        args.get();
+                        return args.tabCompleteDatatype(ForAxis.INSTANCE);
+                    }
+                } else if (action == Action.EXPAND || action == Action.CONTRACT || action == Action.SHIFT) {
+                    if (args.hasExactlyOne()) {
+                        return new TabCompleteHelper()
+                                .append(TransformTarget.getAllNames())
