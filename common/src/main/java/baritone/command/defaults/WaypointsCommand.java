bdc3a7684e85b8b6bf50b4ea2e31a8d2958add6f
LittleSadSheep <2749533087@qq.com>
2026-08-27 20:58:27 +0800

    feat: 支持床点死亡点与自定义路标

--- common/src/main/java/baritone/command/defaults/WaypointsCommand.java
+++ common/src/main/java/baritone/command/defaults/WaypointsCommand.java
@@ -298,3 +298,119 @@ public class WaypointsCommand extends Command {
                             String.format(
                                     "%s%s restore @ %s",
                                     FORCE_COMMAND_PREFIX,
+                                    label,
+                                    waypoint.getCreationTimestamp()
+                            )
+                    )));
+                    logDirect(textComponent);
+                } else if (action == Action.GOAL) {
+                    Goal goal = new GoalBlock(waypoint.getLocation());
+                    baritone.getCustomGoalProcess().setGoal(goal);
+                    logDirect(String.format("Goal: %s", goal));
+                } else if (action == Action.GOTO) {
+                    Goal goal = new GoalBlock(waypoint.getLocation());
+                    baritone.getCustomGoalProcess().setGoalAndPath(goal);
+                    logDirect(String.format("Going to: %s", goal));
+                }
+            }
+        }
+    }
+
+    @Override
+    public Stream<String> tabComplete(String label, IArgConsumer args) throws CommandException {
+        if (args.hasAny()) {
+            if (args.hasExactlyOne()) {
+                return new TabCompleteHelper()
+                        .append(Action.getAllNames())
+                        .sortAlphabetically()
+                        .filterPrefix(args.getString())
+                        .stream();
+            } else {
+                Action action = Action.getByName(args.getString());
+                if (args.hasExactlyOne()) {
+                    if (action == Action.LIST || action == Action.SAVE || action == Action.CLEAR) {
+                        return new TabCompleteHelper()
+                                .append(IWaypoint.Tag.getAllNames())
+                                .sortAlphabetically()
+                                .filterPrefix(args.getString())
+                                .stream();
+                    } else if (action == Action.RESTORE) {
+                        return Stream.empty();
+                    } else {
+                        return args.tabCompleteDatatype(ForWaypoints.INSTANCE);
+                    }
+                } else if (args.has(3) && action == Action.SAVE) {
+                    args.get();
+                    args.get();
+                    return args.tabCompleteDatatype(RelativeBlockPos.INSTANCE);
+                }
+            }
+        }
+        return Stream.empty();
+    }
+
+    @Override
+    public String getShortDesc() {
+        return "Manage waypoints";
+    }
+
+    @Override
+    public List<String> getLongDesc() {
+        return Arrays.asList(
+                "The waypoint command allows you to manage Baritone's waypoints.",
+                "",
+                "Waypoints can be used to mark positions for later. Waypoints are each given a tag and an optional name.",
+                "",
+                "Note that the info, delete, and goal commands let you specify a waypoint by tag. If there is more than one waypoint with a certain tag, then they will let you select which waypoint you mean.",
+                "",
+                "Missing arguments for the save command use the USER tag, creating an unnamed waypoint and your current position as defaults.",
+                "",
+                "Usage:",
+                "> wp [l/list] - List all waypoints.",
+                "> wp <l/list> <tag> - List all waypoints by tag.",
+                "> wp <s/save> - Save an unnamed USER waypoint at your current position",
+                "> wp <s/save> [tag] [name] [pos] - Save a waypoint with the specified tag, name and position.",
+                "> wp <i/info/show> <tag/name> - Show info on a waypoint by tag or name.",
+                "> wp <d/delete> <tag/name> - Delete a waypoint by tag or name.",
+                "> wp <restore> <n> - Restore the last n deleted waypoints.",
+                "> wp <c/clear> <tag> - Delete all waypoints with the specified tag.",
+                "> wp <g/goal> <tag/name> - Set a goal to a waypoint by tag or name.",
+                "> wp <goto> <tag/name> - Set a goal to a waypoint by tag or name and start pathing."
+        );
+    }
+
+    private enum Action {
+        LIST("list", "get", "l"),
+        CLEAR("clear", "c"),
+        SAVE("save", "s"),
+        INFO("info", "show", "i"),
+        DELETE("delete", "d"),
+        RESTORE("restore"),
+        GOAL("goal", "g"),
+        GOTO("goto");
+        private final String[] names;
+
+        Action(String... names) {
+            this.names = names;
+        }
+
+        public static Action getByName(String name) {
+            for (Action action : Action.values()) {
+                for (String alias : action.names) {
+                    if (alias.equalsIgnoreCase(name)) {
+                        return action;
+                    }
+                }
+            }
+            return null;
+        }
+
+        public static String[] getAllNames() {
+            Set<String> names = new HashSet<>();
+            for (Action action : Action.values()) {
+                names.addAll(Arrays.asList(action.names));
+            }
+            return names.toArray(new String[0]);
+        }
+    }
+}
