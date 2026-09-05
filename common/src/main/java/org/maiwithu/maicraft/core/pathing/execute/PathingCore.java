7716c586d6c3ddcdf3c565727a29a0ca13bc01fc
LittleSadSheep <2749533087@qq.com>
2026-09-05 22:33:56 +0800

    refactor: 移除旧导航核心的组件装配

--- common/src/main/java/org/maiwithu/maicraft/core/pathing/execute/PathingCore.java
+++
@@ -1,9 +0,0 @@
-package org.maiwithu.maicraft.core.pathing.execute;
-
-import java.util.Optional;
-import java.util.function.Supplier;
-
-import org.maiwithu.maicraft.core.Constants;
-import org.maiwithu.maicraft.core.pathing.astar.Favoring;
-import org.maiwithu.maicraft.core.pathing.astar.NavPath;
-import org.maiwithu.maicraft.core.pathing.astar.PathCalcResult;
